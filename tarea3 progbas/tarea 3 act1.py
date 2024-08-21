c9f64ed21d3302f51bbebb1812696ee799341e68
nanielpinwin <daniel.cruzj@uanl.edu.mx>
2024-08-20 20:00:29 -0600

    Delete tarea3 progbas directory

--- tarea3 progbas/tarea 3 act1.py
+++
@@ -1,6 +0,0 @@
-# Solicitar nombre y edad al usuario
-nombre = input("Por favor, ingresa tu nombre: ")
-edad = input("Por favor, ingresa tu edad: ")
-
-# Imprimir mensaje de bienvenida
-print(f"Bienvenido, {nombre} te ves muy joven para tenrer {edad} años :0")
