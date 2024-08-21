f8a805a9e5b38649529c755bbb16e9abfb7640ee
nanielpinwin <daniel.cruzj@uanl.edu.mx>
2024-08-20 19:27:46 -0600

    Add files via upload

---
+++ tarea3 progbas/tarea 3 act1.py
@@ -0,0 +1,6 @@
+# Solicitar nombre y edad al usuario
+nombre = input("Por favor, ingresa tu nombre: ")
+edad = input("Por favor, ingresa tu edad: ")
+
+# Imprimir mensaje de bienvenida
+print(f"Bienvenido, {nombre} te ves muy joven para tenrer {edad} años :0")
